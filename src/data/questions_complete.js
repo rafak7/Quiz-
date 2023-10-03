21ce5523b596accd84e8ab03ad8113b7491a9c56
rafak7 <rafael.linoseabra@gmail.com>
2023-10-02 21:05:17 -0300

    Quiz

---
+++ src/data/questions_complete.js
@@ -0,0 +1,72 @@
+const data = [
+    {
+      category: "Rafinha",
+      questions: [
+        {
+          question: "Qual o filme favorito do Rafinha?",
+          options: ["Cidade de Deus", "Carros", "Desafio em Toquio", "Shrek"],
+          answer: "Desafio em Toquio",
+          tip: "Vrum Vrum",
+        },
+        {
+          question: "Qual é carro dos sonhos do Rafinha?",
+          options: ["Civic", "Nivus", "HB20", "Lancer"],
+          answer: "Lancer",
+          tip: "Geralmente usado para Modificações",
+        },
+        {
+          question: "Qual Curso o Rafinha faz na Faculdade?",
+          options: ["Tecnologia da Informação", "Analise Desenvolvimento de Sistemas", "Analise Dsenvolvimento de Sequencias", "Analise Desenvolvimento de Redes"],
+          answer: "Analise Desenvolvimento de Sistemas",
+        },
+        {
+          question: "Nome da namorada do Rafinha",
+          options: ["Maria gabriel luis", "Lulusg", "Malusg", "Maria Luisa"],
+          answer: "Malusg",
+        },
+        {
+          question: "O rafinha tinha quantos anos quando o Rere nasceu?",
+          options: ["4 anos", "5 anos", "10 anos", "7 anos"],
+          answer: "5 anos",
+        },
+      ],
+    },
+    {
+      category: "Malusak",
+      questions: [
+        {
+          question: "Qual foi o primeiro filme que vimos?",
+          options: [
+            "Gato de Botas",
+            "Homem-aranha",
+            "Eternos",
+            "Não! Não Olhe",
+          ],
+          answer: "Eternos",
+        },
+        {
+          question: "Qual foi a musica que tinha na caixa presente que eu te dei?",
+          options: ["Leal", "Profecia", "Os anjos Cantam", "Camisa 10"],
+          answer: "Profecia",
+        },
+        {
+          question: "Qual a MC favorita da Malusg?",
+          options: ["Melody", "Mc Mirella", "Mc byonce", "MC luanna"],
+          answer: "MC luanna",
+        },
+        {
+          question: "Qual a cor de unha que eu mais gosto?",
+          options: ["Branco", "Roxo", "Azul", "Vermelha"],
+          answer: "Branco",
+        },
+        {
+          question:
+            "Qual foi o melhor show que fomos?",
+          options: ["Kayblack", "Jorge e Matheus", "Mc ig", "Veigh"],
+          answer: "Jorge e Matheus",
+        },
+      ],
+    },
+  ];
+  
+  export default data;
